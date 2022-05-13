0f91074e1ae7c82430131d0d2e0b9b8c65c5ea08
Simon Kojima <simon.kojima@donders.ru.nl>
2022-05-13 17:41:58 +0200

    change directory tree

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="markerbci",
-    version="0.0.3",
+    version="0.0.4",
     author="Simon Kojima",
     author_email="simon.kojima@outlook.com",
     description="Handling marker devices for brain-computer interfaces.",
